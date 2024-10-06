031dca4aeb64ed9b88f9d1c1b42ab32af1694093
Ilya <bytskiy.i@gmail.com>
2024-10-07 02:01:13 +0300

    [ADD] Added validation(validators are empty)

---
+++ Library.Application/Authors/Commands/DeleteAuthor/DeleteAuthorCommandValidator.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+
+namespace Library.Application.Authors.Commands.DeleteAuthor;
+
+public class DeleteAuthorCommandValidator: AbstractValidator<DeleteAuthorCommand>
+{
+    
+}
---
+++ Library.Application/Authors/Commands/UpdateAuthor/UpdateAuthorCommandValidator.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+
+namespace Library.Application.Authors.Commands.UpdateAuthor;
+
+public class UpdateAuthorCommandValidator: AbstractValidator<UpdateAuthorCommand>
+{
+    
+}
